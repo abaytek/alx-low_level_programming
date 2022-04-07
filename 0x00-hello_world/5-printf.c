09a2ffb29d4bf43f851b7df42c16b329ca5a39d9
=abaysew <abaysewtekle@gmail.com>
2022-04-07 12:34:51 -0700

    Low level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string using printf built in function
+ * Description: a simple c program to print a string using printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
